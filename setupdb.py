eeb28aa39fb9f8cd7fc51a77696385f7f646f84b
André Quites Ordovás Santos <andrequites@gmail.com>
2024-02-06 18:53:22 -0300

    adicionada insercao de palestrantes

--- setupdb.py
+++ setupdb.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 import models as dbm
+from sqlalchemy import inspect #TODOAQ:
 
 
 # insert SETORES
@@ -16,10 +17,17 @@ listSetores = [
     'COMERCIALIZAÇÃO'
 ]
 
+listPalestrantes = [
+    #NOME, SETORES (indices), AFILIACAO, EXPERIENCIA    
+    ('JERSON KELMAN', [1, 2], 'UFRJ', 51),
+    ('RICHARD LEE HOCHSTETLER', [2], 'INSTITUTO ACENDE BRASIL', 21)
+]
+
 if __name__ == '__main__':
     session = dbm.makeSession()
 
     # remover todos os arquivos previamente existentes
+    num_deleted = 0
     try:
         num_deleted = session.query(dbm.Setor).delete()
         session.commit()
@@ -29,13 +37,47 @@ if __name__ == '__main__':
     if (num_deleted):
         print('{} preexisting rows deleted...'.format(num_deleted))
 
-    for nomeSetor in listSetores:
-        recsetor = dbm.Setor(nome=nomeSetor)
-        print('--> inserting setor "{}"'.format(nomeSetor))
-        recsetor.insert(session)
+    session = dbm.makeSession()
 
-    print('Now commiting insertions to table SETOR...')
-    recsetor.update(session)
+    # inserção dos setores
+    try:
+        for nomeSetor in listSetores:
+            recsetor = dbm.Setor(nome=nomeSetor)
+            recsetor.insert(session)
+
+        session.commit()
+    except:
+        print('Exception inserting sectors!')
+        session.rollback()
+        raise
+
+    # inserção dos palestrantes
+    session = dbm.makeSession()
+
+    try:
+
+        for palestrData in listPalestrantes:
+            pnome = palestrData[0]
+            psetores = [listSetores[i] for i in palestrData[1]]
+            pafil = palestrData[2]
+            pexper = palestrData[3]
+
+            print(f'Inserting palestrante {pnome}...')
+            setoresResult = session.query(dbm.Setor).filter(dbm.Setor.nome.in_(psetores))
+            setores = setoresResult.all()
+            print('setores results = ', setores)
+
+            palestrante = dbm.Palestrante(nome=pnome, experiencia=pexper,
+                                    afiliacao=pafil, listSetores=setores)
+        
+            palestrante.insert(session)
+        session.commit()
+    except:
+        print('Exception inserting Palestrantes!')
+        session.rollback()
+        raise
+
+    
 
 
 
